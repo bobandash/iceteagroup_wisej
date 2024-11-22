44fd78ebce0b509c09767d987c2f8d6eb9f32938
bobandash <brucehsu1126@gmail.com>
2024-11-22 14:20:12 -0800

    Add: initial migration

---
+++ WisejWebApplication3/Migrations/202411222215514_InitialMigration.cs
@@ -0,0 +1,48 @@
+﻿namespace WisejWebApplication3.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Organizations",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Street = c.String(),
+                        City = c.String(),
+                        Country = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Staffs",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        PhoneNumber = c.String(),
+                        Email = c.String(),
+                        Organization_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Organizations", t => t.Organization_Id)
+                .Index(t => t.Organization_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Staffs", "Organization_Id", "dbo.Organizations");
+            DropIndex("dbo.Staffs", new[] { "Organization_Id" });
+            DropTable("dbo.Staffs");
+            DropTable("dbo.Organizations");
+        }
+    }
+}
---
+++ WisejWebApplication3/Models.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.Data.Entity;
+
+// https://www.entityframeworktutorial.net/code-first/code-based-migration-in-code-first.aspx
+
+public class MyDbContext : DbContext
+{
+    public DbSet<Organization> Organizations { get; set; }
+    public DbSet<Staff> Staffs { get; set; }
+}
+
+public class Organization
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Street { get; set; }
+
+    public string City { get; set; }
+
+    public string Country { get; set; }
+
+    public ICollection<Staff> Staff { get; set; }
+}
+
+public class Staff
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public string Email { get; set; }
+
+    public Organization Organization { get; set; }
+}
